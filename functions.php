670e268109e8d98a7130159c16a88b376320bc5a
saiminh <saiminh@gmail.com>
2022-11-03 09:08:10 +0100

    email test

--- functions.php
+++ functions.php
@@ -112,6 +112,6 @@ function remove_jquery_migrate($scripts){
 
 // Change receipient of simple email contact from
 add_filter( 'mcfb_email_to', function ( $to, $form ) {
-  $to = 'saiminh@gmail.com';
+  $to = 'saiminh@gmail.com, simon@floter.ink';
   return $to;
 }, 10, 2 );
