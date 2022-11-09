fa57afa73cf399fbe148a15f6d4b088272f311e7
saiminh <saiminh@gmail.com>
2022-11-09 14:55:25 +0100

    hero-video pattern

--- inc/patterns/formo-hero-video.php
+++ inc/patterns/formo-hero-video.php
@@ -6,16 +6,16 @@ return array(
 	'title'      => __( 'Hero with Video', 'formo2022' ),
 	'categories' => array( 'header' ),
 	'blockTypes' => array( 'core/template-part/header' ),
-	'content'    => '<!-- wp:group {"align":"full","textColor":"background","className":"formo-hero"} -->
-  <div class="wp-block-group alignfull formo-hero has-background-color has-text-color"><!-- wp:video {"className":"formo-hero-video"} -->
-  <figure class="wp-block-video formo-hero-video"><video loop muted src="https://prismic-io.s3.amazonaws.com/formo/7082ed05-190f-4b07-9f9e-a6f6f1df796f_210910_formo_sizzle_home.mp4" ></video></figure>
+	'content'    => '<!-- wp:group {"align":"full","backgroundColor":"formo-cream","textColor":"background","className":"formo-hero"} -->
+  <div class="wp-block-group alignfull formo-hero has-background-color has-formo-cream-background-color has-text-color has-background" id="hero"><!-- wp:video {"id":331,"className":"formo-hero-video"} -->
+  <figure class="wp-block-video formo-hero-video"><video autoplay loop muted src="https://oscarz3.sg-host.com/wp-content/uploads/2022/11/formo-video-homepage-01.mp4" playsinline></video></figure>
   <!-- /wp:video -->
   
   <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"textColor":"formo-cream","className":"formo-hero-text"} -->
   <div class="wp-block-group formo-hero-text has-formo-cream-color has-text-color"><!-- wp:columns {"className":"grid"} -->
-  <div class="wp-block-columns grid"><!-- wp:column {"width":"50%","className":"span9 gridline content-bottom"} -->
-  <div class="wp-block-column span9 gridline content-bottom" style="flex-basis:50%"><!-- wp:heading {"textAlign":"left","level":1} -->
-  <h1 class="has-text-align-left"><span class="smaller">The</span> <br>future <br><span class="smaller">of</span> dairy</h1>
+  <div class="wp-block-columns grid"><!-- wp:column {"width":"50%","className":"span9 gridline content-centered"} -->
+  <div class="wp-block-column span9 gridline content-centered" style="flex-basis:50%"><!-- wp:heading {"textAlign":"left","level":1} -->
+  <h1 class="has-text-align-left"><span class="smaller"><sub>T<span class="wide">h</span>e</sub></span> <br>fut<span class="wide">u</span>re <br><span class="smaller">of</span> <span class="wide">d</span>airy</h1>
   <!-- /wp:heading --></div>
   <!-- /wp:column -->
   
