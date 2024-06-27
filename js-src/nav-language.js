10e7fd72fcf96ea9e6591ca5084957e51f2afcfd
saiminh <saiminh@gmail.com>
2024-06-27 17:31:00 +0200

    language switcher on hover and touchend

--- js-src/nav-language.js
+++ js-src/nav-language.js
@@ -1,6 +1,15 @@
 export function languageSwitcher() {
   const switcher = document.querySelector('.wp-block-polylang-language-switcher');
-  switcher.addEventListener('click', (e) => {
+
+
+  switcher.addEventListener('mouseenter', () => {
+    switcher.classList.toggle('expanded');
+  })
+  switcher.addEventListener('mouseleave', () => {
+    switcher.classList.toggle('expanded');
+  })
+
+  switcher.addEventListener('touchend', (e) => {
     const target = e.target;
     if (target.classList.contains('current-lang') || target.parentElement.classList.contains('current-lang') ) {
       e.preventDefault();
